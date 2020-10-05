b9ce083238350ca67af44a8efca63e0cdf82b7ca
It's Kay <ninjaminja48@gmail.com>
2020-10-05 14:01:16 +0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+var circleX = 50
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(220);
+  background(0, 0,0)
+
+  fill(150,250,250)
+  ellipse(circleX,100,10,15) 
+  
+  circleX = circleX+1
+
+
+  
 }
